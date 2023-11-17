28d9db53e91c478aa88cccc08dee6a48499b70ce
Luis Flores <frko.lc@MacBook-Pro-de-Luis.local>
2023-11-17 20:49:50 +0100

    genero el proyecto

---
+++ src/app/nav-portfolio-luis/nav-portfolio-luis.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-nav-portfolio-luis',
+  templateUrl: './nav-portfolio-luis.component.html',
+  styleUrls: ['./nav-portfolio-luis.component.scss']
+})
+export class NavPortfolioLuisComponent {
+
+}
